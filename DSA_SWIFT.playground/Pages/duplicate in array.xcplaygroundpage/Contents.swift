41a1bb1975f49140032c881d441e0466d14bbee8
prachipb09 <prachipb09@gmail.com>
2024-05-19 00:04:15 +0530

    duplicate in array

--- DSA_SWIFT.playground/Pages/duplicate in array.xcplaygroundpage/Contents.swift
+++ DSA_SWIFT.playground/Pages/duplicate in array.xcplaygroundpage/Contents.swift
@@ -1,3 +1,16 @@
-import UIKit
+import Foundation
+class Solution {
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        var set = Set<Int>()
+        for i in 0..<nums.count {
+            if !set.contains(nums[i]) {
+                set.insert(nums[i])
+            } else {
+                return true
+            }
+        }
+        return false
+    }
+}
 
-var greeting = "Hello, playground"
+print(Solution().containsDuplicate([1,2,3,4,1]))
